224b6eab248c341a97c6b56c404bde74c11796a2
Veljko28 <charlibear28@gmail.com>
2021-09-01 13:38:27 +0200

    adding the template for the project

---
+++ App.js
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import Header from './components/Header';
+
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Header/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
