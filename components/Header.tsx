b1c2fe205026b12040b1152d7f42876bb41a150c
Veljko28 <charlibear28@gmail.com>
2021-09-01 13:37:54 +0200

    Creating header component with not functionality

---
+++ components/Header.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native';
+import logo from '../assets/logo.png';
+import { Ionicons } from '@expo/vector-icons';
+import {blue} from '../constants/CustomColors';
+
+const Header = () => {
+
+  return (
+    <View style={styles.headerContainer}>
+      <Image source={logo} resizeMode="contain" style={styles.logoStyles}/>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity activeOpacity={0.8} style={styles.buttonStyles}>
+          <Ionicons name="search" size={18} color={blue}/>
+        </TouchableOpacity>
+        <TouchableOpacity activeOpacity={0.8} style={styles.buttonStyles}>
+          <Ionicons name="person" size={18} color={blue}/>
+        </TouchableOpacity>
+           <TouchableOpacity activeOpacity={0.8}>
+          <Ionicons name="menu" size={18} color={blue}/>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({ 
+  logoStyles: {
+    width: 110,
+    height: 40,
+    marginTop: 10,
+  },
+  headerContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  buttonContainer: {
+    marginTop: 20,
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  buttonStyles: {
+    marginRight: 15
+  }
+})
+
+export default Header;
