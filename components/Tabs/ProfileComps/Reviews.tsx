a126b2546e508fc6aebd0e8452cec202be587251
Veljko28 <charlibear28@gmail.com>
2021-09-07 17:10:50 +0200

    adding template profile components

---
+++ components/Tabs/ProfileComps/Reviews.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Reviews = () => {
+
+  return (
+    <View>
+      <Text>Reviews</Text>
+    </View>
+  );
+}
+
+export default Reviews;
