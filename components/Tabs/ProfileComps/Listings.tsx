38a399c18339171c41dad8ea1a5e5c1537fbec94
Veljko28 <charlibear28@gmail.com>
2021-09-08 17:45:05 +0200

    mapping the wish list and listing items to phone map component

--- components/Tabs/ProfileComps/Listings.tsx
+++ components/Tabs/ProfileComps/Listings.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import phone from '../../../assets/phone.jpg';
+import MapPhone from '../../Phone/MapPhone';
 
 const Listings = () => {
 
+  const list = [
+    {name: "Pixel 2 Xl", image: phone, description: "This is a listing phone", price: 350},
+    {name: "IPhone 7+", image: phone, description: "This is a listing phone", price: 525},
+    {name: "Huawei P20", image: phone, description: "This is a listing phone", price: 275},
+    {name: "Redmi Note 9", image: phone, description: "This is a listing phone", price: 400},
+  ]
+
   return (
     <View>
-      <Text>Listings</Text>
+      {list.map(x => MapPhone(x))}
     </View>
   );
 }
--- components/Tabs/ProfileComps/WishList.tsx
+++ components/Tabs/ProfileComps/WishList.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import phone from '../../../assets/phone.jpg';
+import MapPhone from '../../Phone/MapPhone';
 
 const WishList = () => {
 
+  const list = [
+    {name: "Pixel 2 Xl", image: phone, description: "This is a Wish List phone", price: 350},
+    {name: "IPhone 7+", image: phone, description: "This is a Wish List phone", price: 525},
+    {name: "Huawei P20", image: phone, description: "This is a Wish List phone", price: 275},
+    {name: "Redmi Note 9", image: phone, description: "This is a Wish List phone", price: 400},
+  ]
+
   return (
     <View>
-      <Text>Wish List</Text>
+      {list.map(x => MapPhone(x))}
     </View>
   );
 }
