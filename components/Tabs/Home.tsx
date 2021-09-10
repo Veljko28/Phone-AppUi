f456cf80e81155092a0bdda74f43d6ade9622f28
Veljko28 <charlibear28@gmail.com>
2021-09-10 18:47:18 +0200

    adjusting components for login and register

--- components/Tabs/Home.tsx
+++ components/Tabs/Home.tsx
@@ -6,10 +6,13 @@ import {white} from '../../constants/CustomColors';
 import PhoneCard from '../PhoneCard';
 import CategoryCircles from '../FrontPage/CategoryCircles';
 import CarouselMain from '../FrontPage/CarouselMain';
+import {useSelector} from 'react-redux';
+import {State} from '../../redux/reduxTypes';
 
 
 
 const Home = () => {
+
   return (
     <View style={styles.container}>
       <Header/> 
