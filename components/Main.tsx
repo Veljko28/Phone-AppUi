353c993b1ac988d75936ca94b9ca3f6f9f12761c
Veljko28 <charlibear28@gmail.com>
2021-09-17 19:52:54 +0200

    adjusting for phone dipslay testing

---
+++ components/Main.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { blue, white } from '../constants/CustomColors';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+
+import Home from './Tabs/Home';
+import Phones from './Tabs/Phones';
+import Bids from './Tabs/Bids';
+import Profile from './Tabs/Profile';
+
+import PhoneDisplay from './PhoneDisplay';
+
+
+
+
+const Tab = createBottomTabNavigator();
+ 
+ const Main = () => {
+   return (
+  <Tab.Navigator screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName="Home"   
+        tabBarOptions={{
+        activeTintColor: white,
+        inactiveTintColor: 'lightgray',
+        activeBackgroundColor: blue,
+        inactiveBackgroundColor: blue,
+            style: {
+                  backgroundColor: blue,
+                  paddingBottom: 3,
+                  display: 'none'
+            }
+          }}
+        >
+          <Tab.Screen name="Home" component={PhoneDisplay} 
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: () => (
+              <Ionicons name="home" size={20} color={white} />
+            ),
+          }}
+          />
+          <Tab.Screen name="Phones" component={Phones} 
+          options={{
+            tabBarLabel: 'Phones',
+            tabBarIcon: () => (
+              <Ionicons 
+              name="phone-portrait" size={20} color={white} />
+            ),
+          }}
+          />
+
+          <Tab.Screen name="Bids" component={Bids} 
+          options={{
+            tabBarLabel: 'Bids',
+            tabBarIcon: () => (
+              <Ionicons 
+              name="time" size={20} color={white} />
+            ),
+          }}
+          />
+
+          <Tab.Screen name="Profile" component={Profile} 
+          options={{
+            tabBarLabel: 'Profile',
+            tabBarIcon: () => (
+              <Ionicons 
+              name="person" size={20} color={white} />
+            ),
+          }}
+          />
+    </Tab.Navigator>
+   );
+ }
+
+export default Main;
