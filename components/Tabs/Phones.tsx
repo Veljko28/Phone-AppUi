36b947c57a29844a8f799004a06cdb1c26a068a8
Veljko28 <charlibear28@gmail.com>
2021-09-19 18:39:08 +0200

    adding stack navigation to phones

--- components/Tabs/Phones.tsx
+++ components/Tabs/Phones.tsx
@@ -1,77 +1,27 @@
 import React from 'react';
-import { View ,Text, StyleSheet, Dimensions, TouchableOpacity } from "react-native";
-import Constants from 'expo-constants';
-import Header from '../Header';
-import {white, blue} from '../../constants/CustomColors';
-import PhoneCard from '../PhoneCard';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import PhoneList from './PhoneStack/PhoneList';
+import PhoneDisplay from './PhoneStack/PhoneDisplay';
+
+
+const Stack = createStackNavigator();
+
 
 const Phones = () => {
-
   return (
-    <View style={styles.container}>
-      <Header/>
-      <Text style={styles.title}>Phones</Text>
-      <View style={styles.cardsContainer}>
-        <PhoneCard/>
-        <PhoneCard/>
-        <PhoneCard/>
-      </View>
-      <View style={styles.cardsContainer}>
-        <PhoneCard/>
-        <PhoneCard/>
-        <PhoneCard/>
-      </View>
-      <View style={styles.cardsContainer}>
-        <PhoneCard/>
-        <PhoneCard/>
-        <PhoneCard/>
-      </View>
-      <View style={styles.cardsContainer}>
-          <TouchableOpacity activeOpacity={0.8} style={styles.pageButton}>
-            <Text style={{color: white}}>1</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity activeOpacity={0.8} style={styles.pageButton}>
-            <Text style={{color: white}}>2</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity activeOpacity={0.8} style={styles.pageButton}>
-            <Text style={{color: white}}>3</Text>
-          </TouchableOpacity>
-      </View>
-    </View>
-  );
+    <Stack.Navigator  screenOptions={{
+      headerShown: false
+    }}
+    initialRouteName="List" 
+    >
+        <Stack.Screen
+          name="List"
+          component={PhoneList}
+        />
+        <Stack.Screen name="Display" component={PhoneDisplay} />
+      </Stack.Navigator>
+  )
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: Constants.statusBarHeight,
-    width:  Dimensions.get('window').width,
-    backgroundColor: white,
-    padding: 8,
-  },
-  title: {
-    textAlign: 'center',
-    color: blue,
-    fontSize: 20,
-    marginTop: 10
-  },
-  cardsContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center'
-  },
-  pageButton: {
-    backgroundColor: blue,
-    color: white,
-    width: 50,
-    borderRadius: 5,
-    padding: 10,
-    margin: 5,
-    textAlign: 'center'
-  }
-});
 
 export default Phones;
