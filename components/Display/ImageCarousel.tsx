67758883e1b08260f431cfd8355e6537de297b68
Veljko28 <charlibear28@gmail.com>
2021-09-19 18:38:13 +0200

    adding dots for image carousel

--- components/Display/ImageCarousel.tsx
+++ components/Display/ImageCarousel.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet, ScrollView, Dimensions, Image} from 'react-native';
 import Constants from 'expo-constants';
+import { blue } from '../../constants/CustomColors';
 
 
 const width = Dimensions.get('window').width;
@@ -11,14 +12,25 @@ const ImageCarousel = () => {
   "https://wirelesswarehouse.ca/wp-content/uploads/2019/08/s-l1600.jpg",
   "https://bigmag.ua/image/cache/catalog/archive/data/catalog/product/704/24/product_image_9924_39511-650x540.jpeg"];
 
+  const [currentImage, changeCurrentImage] = React.useState(0);
+
+  const changeImage = ({nativeEvent}: any) => {
+    const slide = Math.ceil(nativeEvent.contentOffset.x / nativeEvent.layoutMeasurement.width);
+    if (slide != currentImage){
+      changeCurrentImage(slide);
+    }
+  }
 
   return (
     <View style={styles.conatiner}>
-      <ScrollView horizontal pagingEnabled style={styles.scrollStyles} showsHorizontalScrollIndicator={false} >
+      <ScrollView horizontal pagingEnabled style={styles.scrollStyles} showsHorizontalScrollIndicator={false} onScroll={changeImage} >
         {images.map((item, idx) => (
           <Image key={idx} source={{uri: item}} style={styles.scrollStyles} resizeMode="contain"/>
         ))}
       </ScrollView>
+      <Text style={styles.dots}>
+        {images.map((x, idx) => <Text style={{color: idx === currentImage ? blue : "#999"}}>● </Text>)}
+      </Text>
     </View>
   )
 };
@@ -34,6 +46,12 @@ const styles = StyleSheet.create({
     width,
     height: 350
   },
+  dots: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    textAlign: 'center',
+    fontSize: 15
+  }
 });
 
 export default ImageCarousel;
