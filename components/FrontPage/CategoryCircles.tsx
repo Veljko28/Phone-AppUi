a6a32fc1e27e7a61bea5ef4bd6c5bb6ec4eb05f0
Veljko28 <charlibear28@gmail.com>
2021-09-19 18:39:43 +0200

    adding brand images to category circles

--- components/FrontPage/CategoryCircles.tsx
+++ components/FrontPage/CategoryCircles.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import {View, Text, Image, StyleSheet, ScrollView, TouchableOpacity} from 'react-native';
 import {blue} from '../../constants/CustomColors';
-import image from '../../assets/phone.jpg';
+
+import appleLogo from '../../assets/category/apple_logo.png';
+import googleLogo from '../../assets/category/google_logo.png';
+import huaweiLogo from '../../assets/category/huawei_logo.png';
+import samsungLogo from '../../assets/category/samsung_logo.png';
+import redmiLogo from '../../assets/category/redmi_logo.png';
+import honorLogo from '../../assets/category/honor_logo.png';
+
+
+
 
 
 const CategoryCircles = () => {
@@ -12,18 +21,18 @@ const CategoryCircles = () => {
           <View style={styles.circle}>
             <Image source={image} resizeMode="contain" style={styles.imageStyles}/>
           </View>
-          <Text style={{marginLeft: 15}}>{title}</Text>
+          <Text style={{marginLeft: 15, color: blue}}>{title}</Text>
        </TouchableOpacity>
      )
   }
 
 
   return (
-    <ScrollView>
+    <ScrollView horizontal showsHorizontalScrollIndicator={false}>
       <View style={styles.circleContainer}>
       {
-        [{image, title: "Apple"},{image, title: "Google"},{image, title: "Huawei"},
-        {image, title: "Samsung"},{image, title: "Redmi"},{image, title: "Honor"}].map(x => CircleMap(x))
+        [{image: appleLogo, title: "Apple"},{image: googleLogo, title: "Google"},{image: huaweiLogo, title: "Huawei"},
+        {image: samsungLogo, title: "Samsung"},{image: redmiLogo, title: "Redmi"},{image: honorLogo, title: "Honor"}].map(x => CircleMap(x))
       }
       </View>
     </ScrollView>
