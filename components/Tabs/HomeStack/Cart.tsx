b5b2acc19f05262f33ac02d3c26919735552dfb5
Veljko28 <charlibear28@gmail.com>
2021-09-20 17:02:50 +0200

    starting the cart component

--- components/Tabs/HomeStack/Cart.tsx
+++ components/Tabs/HomeStack/Cart.tsx
@@ -1,17 +1,56 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import Constants from 'expo-constants';
+import image from '../../../assets/phone.jpg';
 
-const Cart = () => {
+import {blue, white} from '../../../constants/CustomColors';
+
+const Cart = ({navigation, changeRemoveTabsOnSearch} : {navigation: any, changeRemoveTabsOnSearch: any}) => {
+
+ React.useEffect( () => {
+    changeRemoveTabsOnSearch(true);
+  }, [])
+
+  const itemsInCart = [
+    {image, name: "Pixel 2 XL", price: 350},
+    {image, name: "Pixel 2 XL", price: 350},
+    {image, name: "Pixel 2 XL", price: 350},
+  ]
 
   return (
-    <View>
-      <Text>Cart</Text>
+    <View style={styles.container}> 
+    
+       <View style={styles.header}>
+        <TouchableOpacity activeOpacity={0.9} onPress={() => {  changeRemoveTabsOnSearch(false);                             navigation.navigate("Home") }} style={{marginTop: 10}} >
+          <MaterialIcons name="arrow-back" size={20} color={blue} />
+        </TouchableOpacity>
+        <View>
+          <Text style={styles.title}>Cart</Text>
+        </View>
+        <View> </View>
+      </View>
+
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    backgroundColor: white
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 15,
+    paddingBottom: 0,
+    marginTop: Constants.statusBarHeight
+  },
+  title: {
+    color: blue,
+    fontSize: 25,
+  }
 });
 
 export default Cart;
