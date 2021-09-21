3abad271f253b5940864f1d2cb6a8b249ca57058
Veljko28 <charlibear28@gmail.com>
2021-09-21 16:45:06 +0200

    adding the step indicator styles and settings

---
+++ components/Tabs/HomeStack/CartComps/StepIndicatorSettings.ts
@@ -0,0 +1,23 @@
+export default {
+  stepIndicatorSize: 25,
+  currentStepIndicatorSize:30,
+  separatorStrokeWidth: 2,
+  currentStepStrokeWidth: 3,
+  stepStrokeCurrentColor: '#0cafe5',
+  stepStrokeWidth: 3,
+  stepStrokeFinishedColor: '#0cafe5',
+  stepStrokeUnFinishedColor: '#aaaaaa',
+  separatorFinishedColor:  '#0cafe5',
+  separatorUnFinishedColor: '#aaaaaa',
+  stepIndicatorFinishedColor: '#0cafe5',
+  stepIndicatorUnFinishedColor: '#ffffff',
+  stepIndicatorCurrentColor: '#ffffff',
+  stepIndicatorLabelFontSize: 13,
+  currentStepIndicatorLabelFontSize: 13,
+  stepIndicatorLabelCurrentColor: '#0cafe5',
+  stepIndicatorLabelFinishedColor: '#ffffff',
+  stepIndicatorLabelUnFinishedColor: '#aaaaaa',
+  labelColor: '#999999',
+  labelSize: 13,
+  currentStepLabelColor: '#0cafe5'
+}
