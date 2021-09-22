ad358d39e30c662cdbd2ae0e1f90706c5f2c7d23
Veljko28 <charlibear28@gmail.com>
2021-09-22 20:26:33 +0200

    adding the new cart comps to the main cart component

--- components/Tabs/HomeStack/Cart.tsx
+++ components/Tabs/HomeStack/Cart.tsx
@@ -3,18 +3,24 @@ import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-nati
 import { MaterialIcons } from '@expo/vector-icons';
 import Constants from 'expo-constants';
 import image from '../../../assets/phone.jpg';
+import { useSelector, useDispatch } from 'react-redux';
+import { State } from '../../../redux/reduxTypes';
+import { addCartForm } from '../../../redux/actions/cartActions';
+import { CartForm } from '../../../constants/CustomTypes';
 
 import {blue, white} from '../../../constants/CustomColors';
 
 import CartItemMap from './CartComps/CartItemMap';
 import Delivery from './CartComps/Delivery';
+import OrderSummary from './CartComps/OrderSummary';
+import PaymentMethod from './CartComps/PaymentMethod';
 
 
 import StepIndicator from 'react-native-step-indicator';
 import customStyles from './CartComps/StepIndicatorSettings';
  
-const labels = ["Cart","Delivery Address","Order Summary","Payment Method","Track"];
-const icons = ["shopping-cart", "location-on", "insert-chart", "credit-card", "track-changes"]
+const labels = ["Cart","Delivery Address","Order Summary","Payment Method","Finish"];
+const icons = ["shopping-cart", "location-on", "insert-chart", "credit-card", "check"]
 
 const Cart = ({navigation, changeRemoveTabsOnSearch} : {navigation: any, changeRemoveTabsOnSearch: any}) => {
 
@@ -28,8 +34,19 @@ const Cart = ({navigation, changeRemoveTabsOnSearch} : {navigation: any, changeR
     {image, name: "Pixel 2 XL", price: 350},
     {image, name: "Pixel 2 XL", price: 350},
   ]
+  const [form, changeForm] = React.useState<CartForm>({
+    firstName: "",
+    lastName: "",
+    address: "",
+    town: "",
+    postCode: "",
+    phoneNum: ""
+  })
 
-  const [step, changeStep] = React.useState(0);
+
+
+  const [step, changeStep] = React.useState(3);
+  const dispatch = useDispatch();
 
   return (
     <View style={styles.container}> 
@@ -53,9 +70,13 @@ const Cart = ({navigation, changeRemoveTabsOnSearch} : {navigation: any, changeR
          color={stepStatus === 'finished' ? white : blue} />)}
     />
 
-      {step === 0 ? <CartItemMap itemsInCart={itemsInCart} /> : <Delivery/>}
+      {step === 0 ? <CartItemMap itemsInCart={itemsInCart} /> : step === 1 ? <Delivery form={form} changeForm={(value: CartForm) => changeForm(value)}/> : step === 2 ? <OrderSummary 
+      total={ itemsInCart.reduce((tot, record) =>  tot + record.price,0) }/> : <PaymentMethod/>}
 
-      <TouchableOpacity activeOpacity={0.8} style={styles.button} onPress={() => changeStep(step+1)}>
+      <TouchableOpacity activeOpacity={0.8} style={styles.button} onPress={() => {
+            changeStep(step+1);
+            if (step === 1) dispatch(addCartForm(form));
+        }}>
         <Text style={styles.buttonText}>{step !== 4 ? "Next " : "Finish "} 
         <MaterialIcons name={step !== 4 ? "arrow-forward-ios" : "check"} size={15} color={white} /></Text>
       </TouchableOpacity>
