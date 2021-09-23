149896b0b2f2d71df4b7e472786a8ffdfd23ba7f
Veljko28 <charlibear28@gmail.com>
2021-09-23 17:33:07 +0200

    adding the add to card action

--- redux/actions/cartActions.ts
+++ redux/actions/cartActions.ts
@@ -1,9 +1,15 @@
-import { CartForm } from '../../constants/CustomTypes';
+import { CartForm, Phone } from '../../constants/CustomTypes';
 
 export const ADD_CART_FORM = "ADD_CART_FORM";
+export const ADD_TO_CART = "ADD_TO_CART"; 
 
 
 export const addCartForm = (form: CartForm) => ({
   type: ADD_CART_FORM,
   payload: form
 }) 
+
+export const addToCart = (phone: Phone) => ({
+  type: ADD_TO_CART,
+  payload: phone
+})
