daf9331989e19801bc8823b1ec8f6ab72833fb96
Veljko28 <charlibear28@gmail.com>
2021-09-24 14:21:33 +0200

    clearing and removing actions and reducer function in redux store

--- redux/actions/cartActions.ts
+++ redux/actions/cartActions.ts
@@ -2,6 +2,8 @@ import { CartForm, Phone } from '../../constants/CustomTypes';
 
 export const ADD_CART_FORM = "ADD_CART_FORM";
 export const ADD_TO_CART = "ADD_TO_CART"; 
+export const CLEAR_CART = "CLEAR_CART";
+export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
 
 
 export const addCartForm = (form: CartForm) => ({
@@ -12,4 +14,13 @@ export const addCartForm = (form: CartForm) => ({
 export const addToCart = (phone: Phone) => ({
   type: ADD_TO_CART,
   payload: phone
+})
+
+export const clearCart = () => ({
+  type: CLEAR_CART
+})
+
+export const removeFromCart = (id: number | string) => ({
+  type: REMOVE_FROM_CART,
+  payload: id
 })
--- redux/reducers/cartReducers.ts
+++ redux/reducers/cartReducers.ts
@@ -1,5 +1,6 @@
 import { Action } from '../reduxTypes';
-import { ADD_CART_FORM, ADD_TO_CART } from '../actions/cartActions';
+import { Phone } from '../../constants/CustomTypes';
+import { ADD_CART_FORM, ADD_TO_CART, CLEAR_CART, REMOVE_FROM_CART } from '../actions/cartActions';
 
 const initState = {
   form: {
@@ -17,6 +18,8 @@ const cartReducers = (state = initState, action: Action) => {
   switch (action.type){
     case ADD_CART_FORM: return {...state, form: action.payload};
     case ADD_TO_CART: return {...state, cartItems: [...state.cartItems, action.payload]};
+    case CLEAR_CART: return {...state, cartItems: []};
+    case REMOVE_FROM_CART: return {...state, cartItems: state.cartItems.filter((x: Phone) => x.id !== action.payload)}
     default: return state;
   }
 }
