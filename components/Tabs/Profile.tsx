e8e97c1ef4491b36640267a8466c90d8c4f9d852
Veljko28 <charlibear28@gmail.com>
2021-09-25 18:57:54 +0200

    creating the profile stack

--- components/Tabs/Profile.tsx
+++ components/Tabs/Profile.tsx
@@ -1,136 +1,27 @@
 import React from 'react';
-import Header from '../Header';
-import { white, blue } from '../../constants/CustomColors';
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  Dimensions,
-  StatusBar,
-  ScrollView,
-} from 'react-native';
-import { Avatar, Image } from 'react-native-paper';
-import Constants from 'expo-constants';
-import { IconButton } from 'react-native-paper';
-import { Ionicons } from '@expo/vector-icons';
-import userImage from '../../assets/userprofile.png';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import Listings from './ProfileComps/Listings';
-import Reviews from './ProfileComps/Reviews';
-import WishList from './ProfileComps/WishList';
+import ProfileFront from './ProfileComps/ProfileFront';
+import ProfileSettings from './ProfileComps/ProfileSettings';
+
+
+
+
+const Stack = createStackNavigator();
+
 
 const Profile = () => {
-  const [page, changePage] = React.useState('Listings');
-
-  const pagination = (value: string) => {
-    changePage(value);
-  };
 
   return (
-    <ScrollView style={{ flex: 1, backgroundColor: '#fff' }}>
-      <StatusBar barStyle="light-content" />
-      <View
-        style={{
-          backgroundColor: blue,
-          paddingTop: Constants.statusBarHeight,
-        }}>
-        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-        <TouchableOpacity activeOpacity={0.8}>
-          <IconButton
-            icon={() => (
-              <Ionicons name="mail" style={{ color: white }} size={20} />
-            )}
-            size={30}
-          />
-        </TouchableOpacity>
-          <Text style={styles.userName}>Artisoft</Text>
-        <TouchableOpacity activeOpacity={0.8}>
-          <IconButton
-            icon={() => (
-              <Ionicons
-                name="settings-outline"
-                style={{ color: white }}
-                size={20}
-              />
-            )}
-            size={33}
-          />
-        </TouchableOpacity>
-        </View>
-        <View style={{ alignItems: 'center', marginTop: 30, marginBottom: 30 }}>
-          <Avatar.Image size={150} source={userImage} />
-          <Text style={styles.descriptionText}>Sold: 15</Text>
-          <Text style={styles.descriptionText}>Active Listings: 5</Text>
-          <Text style={styles.descriptionText}>
-            Rating: 4.75{' '}
-            <Ionicons name="star" style={{ color: '#f9ff00' }} size={20} />
-          </Text>
-        </View>
-      </View>
-      <View style={styles.buttonsContainer}>
-        <TouchableOpacity activeOpacity={0.8}>
-          <Text
-            style={page === 'Listings' ? styles.selectedButton : styles.button}
-            onPress={(e) => pagination('Listings')}>
-            Listings
-          </Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity activeOpacity={0.8}>
-          <Text
-            style={page === 'Reviews' ? styles.selectedButton : styles.button}
-            onPress={(e) => pagination('Reviews')}>
-            Reviews
-          </Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity activeOpacity={0.8}>
-          <Text
-            style={page === 'Wish List' ? styles.selectedButton : styles.button}
-            onPress={(e) => pagination('Wish List')}>
-            Wish List
-          </Text>
-        </TouchableOpacity>
-      </View>
-
-      {page === 'Listings' ? (
-        <Listings />
-      ) : page === 'Reviews' ? (
-        <Reviews />
-      ) : (
-        <WishList />
-      )}
-    </ScrollView>
+    <Stack.Navigator  
+    initialRouteName="Front"   
+    screenOptions={{
+      headerShown: false
+    }}>
+      <Stack.Screen name="Front" component={ProfileFront} />
+      <Stack.Screen name="Settings" component={ProfileSettings} />
+    </Stack.Navigator>
   );
-};
-
-const styles = StyleSheet.create({
-  userName: {
-    marginTop: 13,
-    fontSize: 25,
-    color: '#fff',
-    textAlign: 'center',
-  },
-  descriptionText: {
-    marginTop: 10,
-    fontSize: 18,
-    color: '#fff',
-  },
-  buttonsContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    marginTop: 10,
-  },
-  button: {
-    color: blue,
-    fontSize: 15,
-  },
-  selectedButton: {
-    color: '#104c61',
-    fontSize: 15,
-  },
-});
+}
 
 export default Profile;
